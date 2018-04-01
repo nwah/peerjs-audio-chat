582156eef114abcefc78d76451402a2e691af3bb
sergey <sergio_89@ukr.net>
2018-04-01 09:07:10 +0300

    mute added and recorder

--- index.js
+++ index.js
@@ -1,9 +1,17 @@
-var path = require('path');
-var express = require('express');
+const path = require('path');
+const https = require('https');
+const fs = require('fs');
 
-var routes = require('./routes');
+const express = require('express');
+const routes = require('./routes');
+const app = express();
 
-var app = express();
+const options = {
+    key: fs.readFileSync('ssl/server.key'),
+    cert: fs.readFileSync('ssl/server.crt'),
+};
+
+https.createServer(options, app).listen(6767);
 
 app.set('views', path.join(__dirname, 'views'));
 app.set('view engine', 'ejs');
@@ -11,5 +19,11 @@ app.set('view engine', 'ejs');
 app.use(express.static(path.join(__dirname, 'public')));
 app.use('/', routes);
 
-app.listen(6767);
+var PeerServer = require('peer').PeerServer;
+
+var server = PeerServer({
+  port: 9000,
+  ssl: options
+});
+
 console.log('Listening on 6767');
--- public/app.js
+++ public/app.js
@@ -5,11 +5,13 @@ navigator.getUserMedia = (navigator.getUserMedia || navigator.webkitGetUserMedia
 var me = {};
 var myStream;
 var peers = {};
-
-init();
+var muted = false;
+const worker = new Worker('worker/resampler-worker.js');
+//init();
 
 // Start everything up
 function init() {
+  console.log('Init');
   if (!navigator.getUserMedia) return unsupported();
 
   getLocalAudioStream(function(err, stream) {
@@ -28,7 +30,48 @@ function init() {
 // Connect to PeerJS and get an ID
 function connectToPeerJS(cb) {
   display('Connecting to PeerJS...');
-  me = new Peer({key: API_KEY});
+  me = new Peer({ key: API_KEY, 
+                  port: 9000,
+                  debug: 3, 
+                  host: location.hostname, 
+                  secure: true,
+                  config:  {'iceServers': [ 
+                    {url:'stun:stun01.sipphone.com'},
+                    {url:'stun:stun.ekiga.net'},
+                    {url:'stun:stun.fwdnet.net'},
+                    {url:'stun:stun.ideasip.com'},
+                    {url:'stun:stun.iptel.org'},
+                    {url:'stun:stun.rixtelecom.se'},
+                    {url:'stun:stun.schlund.de'},
+                    {url:'stun:stun.l.google.com:19302'},
+                    {url:'stun:stun1.l.google.com:19302'},
+                    {url:'stun:stun2.l.google.com:19302'},
+                    {url:'stun:stun3.l.google.com:19302'},
+                    {url:'stun:stun4.l.google.com:19302'},
+                    {url:'stun:stunserver.org'},
+                    {url:'stun:stun.softjoys.com'},
+                    {url:'stun:stun.voiparound.com'},
+                    {url:'stun:stun.voipbuster.com'},
+                    {url:'stun:stun.voipstunt.com'},
+                    {url:'stun:stun.voxgratia.org'},
+                    {url:'stun:stun.xten.com'},
+                    {
+                      url: 'turn:numb.viagenie.ca',
+                      credential: 'muazkh',
+                      username: 'webrtc@live.com'
+                    },
+                    {
+                      url: 'turn:192.158.29.39:3478?transport=udp',
+                      credential: 'JZEOEt2V3Qb0y27GRntt2u2PAYA=',
+                      username: '28224511:1379330808'
+                    },
+                    {
+                      url: 'turn:192.158.29.39:3478?transport=tcp',
+                      credential: 'JZEOEt2V3Qb0y27GRntt2u2PAYA=',
+                      username: '28224511:1379330808'
+                    }
+                  ]}
+                });
 
   me.on('call', handleIncomingCall);
   
@@ -109,7 +152,18 @@ function getLocalAudioStream(cb) {
 
     function success(audioStream) {
       display('Microphone is open.');
+      
       myStream = audioStream;
+
+      context = new AudioContext();
+            
+      var source = context.createMediaStreamSource(myStream);
+      var bufferSize = 0; // let implementation decide
+      recorder = context.createScriptProcessor(bufferSize, 1, 1);
+      recorder.onaudioprocess = onAudio;
+      source.connect(recorder);
+      recorder.connect(context.destination);
+
       if (cb) cb(null, myStream);
     },
 
@@ -120,6 +174,46 @@ function getLocalAudioStream(cb) {
   );
 }
 
+function onAudio(e) {
+  var left = e.inputBuffer.getChannelData(0);
+
+  worker.postMessage({cmd: "resample", buffer: left});
+
+  drawBuffer(left);
+}
+
+function convertFloat32ToInt16(buffer) {
+  var l = buffer.length;
+  var buf = new Int16Array(l);
+  while (l--) {
+      buf[l] = Math.min(1, buffer[l]) * 0x7FFF;
+  }
+  return buf.buffer;
+}
+
+//https://github.com/cwilso/Audio-Buffer-Draw/blob/master/js/audiodisplay.js
+function drawBuffer(data) {
+  var canvas = document.getElementById("canvas"),
+      width = canvas.width,
+      height = canvas.height,
+      context = canvas.getContext('2d');
+
+  context.clearRect (0, 0, width, height);
+  var step = Math.ceil(data.length / width);
+  var amp = height / 2;
+  for (var i = 0; i < width; i++) {
+      var min = 1.0;
+      var max = -1.0;
+      for (var j = 0; j < step; j++) {
+          var datum = data[(i * step) + j];
+          if (datum < min)
+              min = datum;
+          if (datum > max)
+              max = datum;
+      }
+      context.fillRect(i, (1 + min) * amp, 1, Math.max(1, (max - min) * amp));
+  }
+}
 
 
 ////////////////////////////////////
@@ -142,5 +236,45 @@ function unsupported() {
 }
 
 function display(message) {
-  $('<div />').html(message).appendTo('#display');
+  $('<div/>').html(message).appendTo('#display');
 }
+
+$(document).ready(function() {
+  console.log( "ready!" );
+
+  const contextSampleRate = (new AudioContext()).sampleRate;
+  const resampleRate = contextSampleRate;
+  
+  var bStream;
+  var recorder;
+
+  worker.postMessage({ cmd:"init", from:contextSampleRate, to: resampleRate });
+
+  worker.addEventListener('message', function (e) {
+    if (bStream && bStream.writable)
+        bStream.write(convertFloat32ToInt16(e.data.buffer));
+  }, false);
+
+
+  $('#mute').click(function(){
+    console.log('mute');
+
+    if(!muted){
+      myStream.getAudioTracks()[0].enabled = true;
+      muted = true;
+    } else{
+      myStream.getAudioTracks()[0].enabled = false;
+      muted = false;
+    }
+  })
+
+  $('#start').click(function(){
+    client = new BinaryClient(`wss://${location.hostname}:8080`);
+    client.on('open', function () {
+        bStream = client.createStream({sampleRate: resampleRate, userName: "admin" });
+    });
+
+    init();
+  });
+});
+
